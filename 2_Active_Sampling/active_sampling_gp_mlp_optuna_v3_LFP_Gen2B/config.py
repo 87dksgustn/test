e280fffd6130fdd08d500dd5cceb3b79cc9cd7a6
unknown <hyunsoo.an>
2026-08-18 13:47:57 +0900

    update: apply latest changes

--- 2_Active_Sampling/active_sampling_gp_mlp_optuna_v3_LFP_Gen2B/config.py
+++ 2_Active_Sampling/active_sampling_gp_mlp_optuna_v3_LFP_Gen2B/config.py
@@ -4,7 +4,7 @@
 # User-editable configuration
 # ============================================================
 
-INPUT_CSV = "Itr_5_Dataset.csv"
+INPUT_CSV = "Itr_7_Dataset.csv"
 FINAL_TEST_CSV = "z_Final_Dataset.csv"
 
 # Base continuous columns (original features)
